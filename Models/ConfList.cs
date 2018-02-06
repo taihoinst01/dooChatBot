b2fb2f81cbcf91c6494993739b6de3be4e9e9fb3
junhpark <junhpark@taihoinst.com>
2018-02-06 13:18:13 +0900

    init commit

---
+++ Models/ConfList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DooChatBot.Models
+{
+    public class ConfList
+    {
+        public string cnfType;
+        public string cnfNm;
+        public string cnfValue;
+    }
+}
